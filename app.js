c8dc7088abad985e9d9b53b98fc2a673462f8087
60936 <studio34pl@gmail.com>
2024-01-21 09:37:03 +0800

    r

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/api/people', people)
-app.use('/login', auth)
+
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
